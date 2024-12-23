aecf636e2db75656d0691f0f389bec7602ab9f6e
Martin Lindberg <30564420+fartinmartin@users.noreply.github.com>
2024-12-22 19:21:42 -0800

    update tool

--- www/main.js
+++ www/main.js
@@ -1,84 +1,68 @@
-const form = document.forms.font;
+const ioForm = document.forms.io;
+const fonts = new Set(); // should probs be a map?
 
-let fontName = '';
-let glyphs = [];
-let unitsPerEm = 0;
+ioForm.file.onchange = async ({ target }) => {
+  for (const file of target.files) {
+    const font = await loadFont(file);
+    const metrics = getMetrics(font);
+    fonts.add({ fileName: file.name, ...metrics });
+  }
 
-form.file.onchange = ({ target }) =>
-  compute(target.files[0], target.files[0].name);
+  render(fonts);
+};
 
-form.json.onclick = () => downloadJSON(fontName, glyphs, unitsPerEm);
-form.jsx.onclick = () => downloadJSX(fontName, glyphs, unitsPerEm);
+ioForm.jsx.onclick = () => downloadJSX();
 
-const fontData = sessionStorage.getItem('fontData');
-if (fontData) {
-  const font = base64ToArrayBuffer(fontData);
-  loadFont(font);
-}
+const fontsForm = document.forms.fonts;
+
+fontsForm.onchange = () => {
+  const selected = fontsForm.fonts.value;
+  const font = [...fonts].find((i) => i.fontName === selected);
+  if (font) renderGlyphGrid(font);
+};
+
+setDisabled(true);
+loadStorage();
 
 //
 
-async function compute(file, name) {
-  const isWoff2 = name.endsWith('.woff2');
+async function loadFont(file) {
+  const isWoff2 = file.name.endsWith('.woff2');
   if (isWoff2) await loadWAWOFF2();
 
   try {
     const data = await file.arrayBuffer();
+    const base64String = arrayBufferToBase64(data);
 
-    try {
-      const base64String = arrayBufferToBase64(data);
-      sessionStorage.setItem('fontData', base64String);
-    } catch (err) {
-      print(err.toString());
-    }
+    const fontMap = JSON.parse(sessionStorage.getItem('fonts') || '{}');
+    fontMap[file.name] = `fontData_${file.name}`;
+    sessionStorage.setItem('fonts', JSON.stringify(fontMap));
+    sessionStorage.setItem(`fontData_${file.name}`, base64String);
 
-    const font = isWoff2 ? Module.decompress(data) : data;
-    loadFont(font);
+    return isWoff2 ? Module.decompress(data) : data;
   } catch (err) {
     print(err.toString());
-    console.error(err);
+    console.log(err);
     throw err;
   }
 }
 
-function loadFont(font) {
+function getMetrics(font) {
   try {
     const parsed = opentype.parse(font, { lowMemory: true });
     if (!parsed) throw new Error(`error parsing font!`);
 
-    fontName = getFontName(parsed);
-    unitsPerEm = getUnitsPerEm(parsed);
-    glyphs = getGlyphs(parsed);
-
-    renderGrid(glyphs);
-    setDisabled(false);
-    setFont(parsed.names.fontFamily.en, parsed.names.fontSubfamily.en);
-
-    console.log(parsed.names);
-
-    print([`loaded: ${fontName}`, `unitsPerEm: ${unitsPerEm}`].join(' | '));
+    return {
+      fontName: parsed.names.postScriptName.en,
+      fontFamily: parsed.names.fontFamily.en,
+      fontStyle: parsed.names.fontSubfamily.en,
+      unitsPerEm: parsed.unitsPerEm,
+      glyphs: getGlyphs(parsed),
+    };
   } catch (error) {
     print(error.toString().toLowerCase());
     setDisabled(true);
   }
-}
-
-function setDisabled(value) {
-  form.json.disabled = value;
-  form.jsx.disabled = value;
-}
-
-function setFont(fontName, style) {
-  document.documentElement.style.setProperty('--glyph-font', fontName);
-  document.documentElement.style.setProperty('--glyph-style', style);
-}
-
-function getFontName(font) {
-  return font.names.postScriptName.en;
-}
-
-function getUnitsPerEm(font) {
-  return font.unitsPerEm;
 }
 
 function getGlyphs(font) {
@@ -101,19 +85,107 @@ function getGlyphs(font) {
 
 //
 
-function generateFontMetrics(glyphs, unitsPerEm) {
-  const charWidths = {};
+function loadStorage() {
+  const fontMap = JSON.parse(sessionStorage.getItem('fonts') || '{}');
 
-  for (const glyph of glyphs) {
-    charWidths[glyph.unicode] = glyph.width;
+  for (const fileName in fontMap) {
+    const fontKey = fontMap[fileName];
+    const fontData = sessionStorage.getItem(fontKey);
+
+    if (fontData) {
+      const font = base64ToArrayBuffer(fontData);
+      const metrics = getMetrics(font);
+      fonts.add(metrics);
+    }
   }
 
+  render(fonts);
+}
+
+function removeFontFromStorage(fileName) {
+  const fontMap = JSON.parse(sessionStorage.getItem('fonts') || '{}');
+  const fontKey = fontMap[fileName];
+
+  if (fontKey) {
+    sessionStorage.removeItem(`fontData_${fileName}`);
+    delete fontMap[fileName];
+    sessionStorage.setItem('fonts', JSON.stringify(fontMap));
+  }
+}
+
+//
+
+function render(fonts) {
+  const fontsArray = [...fonts];
+  renderFontList(fontsArray);
+
+  const selectedFont = fontsArray[0];
+  if (selectedFont) {
+    fontsForm.fonts.value = selectedFont.fontName;
+    renderGlyphGrid(selectedFont);
+  } else {
+    clearGlyphGrid();
+  }
+}
+
+function setDisabled(value) {
+  ioForm.jsx.disabled = value;
+}
+
+function setFont({ fontName, fontFamily, fontStyle }) {
+  document.documentElement.style.setProperty('--glyph-font', fontFamily);
+
+  const combo = [fontName, fontFamily, fontStyle].join(' ').toLowerCase();
+  let style = 'normal';
+  if (combo.includes('italic')) style = 'italic';
+  if (combo.includes('oblique')) style = 'oblique';
+
+  document.documentElement.style.setProperty('--glyph-style', style);
+
+  const weight = getFontWeight(combo);
+  document.documentElement.style.setProperty('--glyph-weight', weight);
+}
+
+function getFontWeight(fontName) {
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/font-weight
+  const weightMapping = {
+    thin: 100,
+    'extra light': 200,
+    light: 300,
+    normal: 400,
+    regular: 400,
+    medium: 500,
+    'semi bold': 600,
+    bold: 700,
+    'extra bold': 800,
+    black: 900,
+    'extra black': 950,
+    bolder: 'bolder',
+    lighter: 'lighter',
+  };
+
+  const weightRegex =
+    /\b(thin|extra[-\s]light|light|normal|regular|medium|semi[-\s]bold|bold|extra[-\s]bold|black|extra[-\s]black|bolder|lighter|\d{3})\b/i;
+
+  const match = fontName.match(weightRegex);
+
+  if (match) {
+    const weight = match[0].toLowerCase().replace('-', ' ');
+    return weightMapping[weight] || parseInt(weight, 10);
+  }
+
+  return 400;
+}
+
+//
+
+function generateFontMetrics({ glyphs, unitsPerEm }) {
   const widthGroups = {};
   let totalWidth = 0;
   let charCount = 0;
 
-  const sortedChars = Object.entries(charWidths)
-    .map(([char, width]) => [parseInt(char, 10), width])
+  const sortedChars = glyphs
+    .map(({ unicode, width }) => [parseInt(unicode, 10), width])
     .sort((a, b) => a[0] - b[0]);
 
   for (const [charCode, width] of sortedChars) {
@@ -135,7 +207,10 @@ function generateFontMetrics(glyphs, unitsPerEm) {
   }
 
   // 120 is charCode for 'x', 88 for 'X'
-  const defaultWidth = charWidths['120'] || charWidths['88'] || 500;
+  const defaultWidth =
+    glyphs.find((g) => g.unicode === 120)?.width ||
+    glyphs.find((g) => g.unicode === 88)?.width ||
+    500;
 
   return {
     unitsPerEm,
@@ -146,20 +221,69 @@ function generateFontMetrics(glyphs, unitsPerEm) {
 
 //
 
-function downloadJSON(name, glyphs, unitsPerEm) {
-  const data = generateFontMetrics(glyphs, unitsPerEm);
-  const json = JSON.stringify(data);
-  downloadFile(json, `${name}.json`);
+function getData() {
+  const fontsArray = [...fonts];
+
+  const data = fontsArray.reduce((obj, font) => {
+    return { ...obj, [toPascalCase(font.fontName)]: generateFontMetrics(font) };
+  }, {});
+
+  return {
+    name: fonts.size > 1 ? 'font-metrics' : fontsArray[0].fontName,
+    content: JSON.stringify(data),
+  };
 }
 
-function downloadJSX(name, glyphs, unitsPerEm) {
-  const data = generateFontMetrics(glyphs, unitsPerEm);
-  const content = `{\n\t"${toPascalCase(name)}": ${JSON.stringify(data)}\n}`;
+function downloadJSX() {
+  const { content, name } = getData();
   downloadFile(content, `${name}.jsx`, 'text/plain');
 }
 
-function renderGrid(glyphs) {
-  const grid = document.getElementById('glyph-grid');
+//
+
+function renderFontList(fonts) {
+  const list = document.getElementById('font-list');
+  list.innerHTML = '';
+
+  fonts.forEach((font, index) => {
+    const fontRadio = document.createElement('input');
+    fontRadio.type = 'radio';
+    fontRadio.name = 'fonts';
+    fontRadio.id = `font-${index}`;
+    fontRadio.value = font.fontName;
+
+    const fontLabel = document.createElement('label');
+    fontLabel.htmlFor = `font-${index}`;
+    fontLabel.tabIndex = 0;
+    const fontSpan = document.createElement('span');
+    fontSpan.textContent = font.fontName;
+    fontLabel.appendChild(fontSpan);
+
+    // Create the delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '×';
+    deleteButton.title = 'remove';
+    deleteButton.type = 'button';
+    deleteButton.dataset.value = fontRadio.value;
+    deleteButton.addEventListener('click', handleRemove);
+
+    const fontItem = document.createElement('div');
+    fontItem.classList.add('font-item');
+    fontItem.appendChild(fontRadio);
+    fontItem.appendChild(fontLabel);
+    fontLabel.appendChild(deleteButton);
+
+    list.appendChild(fontItem);
+  });
+
+  setDisabled(fonts.length === 0);
+}
+
+function renderGlyphGrid(font) {
+  const grid = clearGlyphGrid();
+
+  const glyphs = font.glyphs;
+  setFont(font);
 
   glyphs.forEach(({ name, unicode, width, index }) => {
     const cell = document.createElement('div');
@@ -168,7 +292,17 @@ function renderGrid(glyphs) {
 
     const glyphEl = document.createElement('div');
     glyphEl.classList.add('glyph');
-    glyphEl.textContent = String.fromCharCode(unicode);
+    const string = String.fromCharCode(unicode);
+
+    if (/\s/.test(string)) {
+      const span = document.createElement('span');
+      span.classList.add('whitespace');
+      span.textContent = string;
+      glyphEl.innerHTML = '';
+      glyphEl.appendChild(span);
+    } else {
+      glyphEl.textContent = string;
+    }
 
     const indexEl = document.createElement('div');
     indexEl.classList.add('index');
@@ -189,6 +323,22 @@ function renderGrid(glyphs) {
 
     grid.appendChild(cell);
   });
+}
+
+function handleRemove({ target }) {
+  const name = target.dataset.value;
+  const font = [...fonts].find((i) => i.fontName === name);
+  if (font) {
+    fonts.delete(font);
+    removeFontFromStorage(font.fileName);
+    render(fonts);
+  }
+}
+
+function clearGlyphGrid() {
+  const grid = document.getElementById('glyph-grid');
+  grid.innerHTML = '';
+  return grid;
 }
 
 //
